bf684fc2cc8f454518509762f0f957cbe62c5246
Zsombor <birozsombor4@gmail.com>
2020-09-02 11:37:51 +0200

    inital commit

---
+++ src/main/java/com/birozsombor4/springrestapitemplate/SpringRestApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.birozsombor4.springrestapitemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiTemplateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRestApiTemplateApplication.class, args);
+  }
+
+}
